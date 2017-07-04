7dcc6b63bde091bd810a147ff2d0508c76639bf5
Bon Osonwanne <nosonwan@gmail.com>
2017-07-03 22:36:17 -0700

    updated settings again

--- smallplexsites/settings/base.py
+++ smallplexsites/settings/base.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.treebeard',
-    'medCE.delib'
     # 'django.contrib.sites',
 
     'bootstrap_themes',
