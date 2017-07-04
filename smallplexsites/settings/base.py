48d80b9861b8cc03521a9c6196cb2c95601da27d
Bon Osonwanne <nosonwan@gmail.com>
2017-07-03 22:18:41 -0700

    added django-treebeard to settings

--- smallplexsites/settings/base.py
+++ smallplexsites/settings/base.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.treebeard',
     # 'django.contrib.sites',
 
     'bootstrap_themes',
